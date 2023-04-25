e0762134884559c56ce3cbbe6d9a4b48173f9a5d
vi <vitalya.permyakov155@gmail.com>
2023-04-25 18:25:26 +0700

    positions animation

--- src/components/HomeMobile/componentsMobile/ModelsArray.tsx
+++ src/components/HomeMobile/componentsMobile/ModelsArray.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 import falcon_webp from '../../../assets/images/models/falcon.webp'
 import eagle_webp from '../../../assets/images/models/eagle.webp'
@@ -47,18 +47,21 @@ export default function ModelsArray() {
       return 1.5 + (distanceFromMidpoint / midpoint) * 0.5;
     }
   }
+
   const to = (i: number) => ({
-    x: Math.round(window.innerWidth / 3 * (Math.cos(theta(i + 2, 8)))),
-    y: Math.round(window.innerWidth / 5 * (Math.sin(theta(i + 2, 8)))),
+    x: Math.round(window.innerWidth / 3 * (Math.cos(theta(i + currentImg + 2, 8)))),
+    y: Math.round(window.innerWidth / 5 * (Math.sin(theta(i + currentImg + 2, 8)))),
     rot: 0,
-    scale: getEndpoint(i, 8) / 2.5,
+    // scale: getEndpoint(i, 8) / 2.5,
+    scale: 1,
     delay: i * 100,
   })
   const from = (_i: number) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 })
   // This is being used down there in the view, it interpolates rotation and scale into a css transform
+  // const trans = (x: number, y: number, r: number, s: number) => `perspective(1500px) translate3d(${x}, ${y}, 0px) scale(${s})`
   const trans = (r: number, s: number) => `perspective(1500px) scale(${s})`
 
-  const [gone] = useState(() => new Set()) // The set flags all the cards that are flicked out
+  const [currentImg, setCurrentImg] = useState(0) // The set flags all the cards that are flicked out
 
   const [grab, setGrab] = useState(false)
 
@@ -67,9 +70,22 @@ export default function ModelsArray() {
     from: from(i),
   })) // Create a bunch of springs using the helpers above
 
+  function compare(v1: number, v2:number): boolean {
+    let value = v1
+    if (value >= 900) {
+      value = v1 - 900
+    }
+    if (value <= -900) {
+      value = v1 + 900
+    }
+    return value >= v2
+  }
+
+  const spinRef = useRef(0)
+
   // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
   const bind = useDrag(({ args: [index], down, movement: [mx], direction: [xDir], velocity }) => {
-    console.log(mx)
+    // console.log(mx)
 
     if (down) {
       setGrab(true)
@@ -77,28 +93,70 @@ export default function ModelsArray() {
       setGrab(false)
     }
 
-    const trigger = (mx > 20) || (mx < -20) // If you flick hard enough it should trigger the card to fly out
-    const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
-    if (!down && trigger) gone.add(index) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+    spinRef.current = spinRef.current - mx/100
+    if (spinRef.current >= 900) {
+      spinRef.current = spinRef.current - Math.abs(spinRef.current)
+    }
+    if (spinRef.current <= -900) {
+      spinRef.current = spinRef.current + Math.abs(spinRef.current)
+    }
+
+    // console.log(spinRef.current)
+
+
+    compare(spinRef.current, 800) ? 
+    setCurrentImg(0) :
+    compare(spinRef.current, 700) ?
+    setCurrentImg(7) :
+    compare(spinRef.current, 600) ?
+    setCurrentImg(6) :
+    compare(spinRef.current, 500) ?
+    setCurrentImg(5) :
+    compare(spinRef.current, 400) ?
+    setCurrentImg(4) :
+    compare(spinRef.current, 300) ?
+    setCurrentImg(3) :
+    compare(spinRef.current, 200) ?
+    setCurrentImg(2) :
+    compare(spinRef.current, 100) ?
+    setCurrentImg(1) :
+    compare(spinRef.current, 0) ?
+    setCurrentImg(0) :
+    compare(spinRef.current, -100) ?
+    setCurrentImg(7) :
+    compare(spinRef.current, -200) ?
+    setCurrentImg(6) :
+    compare(spinRef.current, -300) ?
+    setCurrentImg(5) :
+    compare(spinRef.current, -400) ?
+    setCurrentImg(4) :
+    compare(spinRef.current, -500) ?
+    setCurrentImg(3) :
+    compare(spinRef.current, -600) ?
+    setCurrentImg(2) :
+    compare(spinRef.current, -700) ?
+    setCurrentImg(1) :
+    setCurrentImg(0)
+
+    console.log(currentImg)
+
     api.start(i => {
-      if (index !== i) return // We're only interested in changing spring-data for the current spring
-      const isGone = gone.has(index)
-      const x = isGone ? (200 + window.innerWidth) * dir : down ? mx : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
-      const rot = mx / 100 + (isGone ? dir * 10 * velocity[0] : 0) // How much the card tilts, flicking it harder makes it rotate faster
-      const scale = down ? 1.1 : 1 // Active cards lift up a bit
+      const x = down ? Math.round(window.innerWidth / 3 * (Math.cos(theta(i + spinRef.current/100 + 2, 8)))) : Math.round(window.innerWidth / 3 * (Math.cos(theta(i + currentImg + 2, 8))))
+      const y = down ? Math.round(window.innerWidth / 5 * (Math.sin(theta(i + spinRef.current/100 + 2, 8)))) : Math.round(window.innerWidth / 5 * (Math.sin(theta(i + currentImg + 2, 8))))
+      const scale = 1
       return {
         x,
-        rot,
+        y,
         scale,
         delay: undefined,
-        config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 },
+        config: { friction: 50, tension: down ? 800 : 500},
       }
     })
-    if (!down && gone.size === cards.length)
+
+    if (!down)
       setTimeout(() => {
-        gone.clear()
         api.start(i => to(i))
-      }, 600)
+      }, 100)
   })
 
   return (
